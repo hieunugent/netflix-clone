73db9aeef41bbdb37f8611dc4fed274654300b41
hieunugent <hieunugent@gmail.com>
2020-10-20 23:25:21 -0700

    add new file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import Jumbotron from './components/jumbotron';
 
 function App() {
   return (
     <div className="app">
-   <p>Hello to app</p>
+   <Jumbotron/>
     </div>
   );
 }
