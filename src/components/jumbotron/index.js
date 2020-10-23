b07279be8297c457c0eb67dcb1dfe69f013450a3
hieunugent <hieunugent@gmail.com>
2020-10-23 00:12:34 -0700

    add style for global

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Container,Inner , Pane, Title, SubTitle, Image} from "./styles/Jumbotron";
+import { Container,Inner ,Item,  Pane, Title, SubTitle, Image} from "./styles/Jumbotron";
 export default function Jumbotron({children, direction='row',...resProps}) {
   return (
-    <Inner direction={direction}>
-     {children}
-    </Inner>
+    <Item direction={direction}>
+      <Inner>{children}</Inner>
+    </Item>
   );
 }
 Jumbotron.Container= function JumbotronContainer({ children, ...restProps}){
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {GlobalStyles} from './global-styles';
+ReactDOM.render(<>
 
-ReactDOM.render(
-  
+   <GlobalStyles/>
     <App />
- ,
+</> ,
   document.getElementById('root')
 );
 
